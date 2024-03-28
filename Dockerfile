43d0499fb0d7f1685c8c37e7b4a7a9fb4f1ae46d
Scot Curry <scotcurry4@gmail.com>
2024-03-28 18:52:32 -0400

    Added logging

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,10 @@ EXPOSE 8000
 
 WORKDIR /app
 COPY ../requirements.txt requirements.txt
-RUN python -m pip install --upgrade pip
-RUN pip3 install -r requirements.txt
+# See Datadog Static Analysis -- https://docs.datadoghq.com/code_analysis/static_analysis_rules/docker-best-practices/pip-pin-versions/
+RUN python3 -m pip install --upgrade pip==24.0 && python3 -m pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 RUN useradd appuser && chown -R appuser /app
 USER appuser
-CMD exec ddtrace-run gunicorn -b :8000 --workers 1 --threads 8 --timeout 0 app:app
+  CMD exec ddtrace-run gunicorn -b :8000 --workers 1 --threads 8 --timeout 0 app:app
--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 import datetime
+import logging
 
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 application = app
 
+logger = logging.getLogger('curryware-front-end')
+consoleOutputHandler = logging.StreamHandler()
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+consoleOutputHandler.setFormatter(formatter)
+logger.addHandler(consoleOutputHandler)
+logger.info('---curryware-front-end start---')
+
 
 @app.route('/')
 def hello_world():
-    print('Launching app!')
+    logger.info('Launching app!')
     current_time = datetime.datetime.now()
     user_agent = request.headers.get('User-Agent')
+    logger.info('User agent: {}'.format(user_agent))
     return render_template('index.html', title='Index Page', current_time=current_time,
                            user_agent=user_agent)
 
