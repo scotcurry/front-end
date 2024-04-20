d135766b9c379828c38c9fda3bba45774b8cf141
Scot Curry <scotcurry4@gmail.com>
2024-04-20 11:00:50 -0400

    Updated index.html formatting, and gunicorn.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import datetime
 import logging
 
 from flask import Flask, render_template, request
+from ddtrace import tracer
 from ddtrace.runtime import RuntimeMetrics
 
 RuntimeMetrics.enable()
@@ -10,12 +11,15 @@ RuntimeMetrics.enable()
 app = Flask(__name__)
 application = app
 
+FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
+          '[dd.service=%(dd.service)s dd.env=%(dd.env)s '
+          'dd.version=%(dd.version)s '
+          'dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
+          '- %(message)s')
+
+logging.basicConfig(format=FORMAT)
 logger = logging.getLogger('curryware-front-end')
-consoleOutputHandler = logging.StreamHandler()
 logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-consoleOutputHandler.setFormatter(formatter)
-logger.addHandler(consoleOutputHandler)
 logger.info('---curryware-front-end start---')
 
 
@@ -25,7 +29,7 @@ def index_page():
     current_time = datetime.datetime.now()
     local_time = current_time.replace(tzinfo=datetime.timezone.utc).astimezone(tz=None)
     user_agent = request.headers.get('User-Agent')
-    logger.info('User agent: {}'.format(user_agent))
+    # logger.info('User agent: {}'.format(user_agent))
     current_date = '{}/{}/{}'.format(str(local_time.month), str(local_time.day), str(local_time.year),
                                      str(local_time.year))
     current_time = '{}:{}:{}'.format(str(local_time.hour), str(local_time.minute),
@@ -37,6 +41,3 @@ def index_page():
                            current_date=current_date, user_agent=user_agent,
                            logs_injection_variable=log_environment_variable)
 
-
-# if __name__ == '__main__':
-#     app.run()
